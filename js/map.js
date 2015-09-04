2cd8682ae81b25dc4de96011494c908f1c1dfe30
Mauricio Miranda <maurimiranda@gmail.com>
2015-09-04 16:20:07 -0300

    Get tasks data dynamically from hot task manager

--- js/map.js
+++ js/map.js
@@ -2,6 +2,22 @@ $(function() {
   // Initialize map
   var map = L.map('map');
 
+  // Define markers style
+  var marker = {
+    icon: L.icon({
+      iconUrl: 'img/icon.png',
+      iconSize: [50, 50]
+    })
+  };
+
+  // Create GeoJSON layer
+  var featuresLayer = L.geoJson(null, {
+    pointToLayer: function (feature, latlng) {
+      return L.marker(latlng, marker);
+    },
+    onEachFeature: onEachFeature
+  }).addTo(map);
+
   // Create info control
   var info = L.control();
   info.onAdd = function (map) {
@@ -10,6 +26,7 @@ $(function() {
   };
   info.update = function (feature) {
     if (feature) {
+      var value = feature.properties.done + feature.properties.validated;
       $(this._div).empty();
       $(this._div).append(
         $('<p></p>')
@@ -19,9 +36,9 @@ $(function() {
         $('<div class="progress"></div>')
           .append(
             $('<div class="progress-bar" role="progressbar"></div>')
-              .attr('aria-valuenow', feature.properties.status)
-              .css('width', feature.properties.status + '%')
-              .append(feature.properties.status + '%')
+              .attr('aria-valuenow', value)
+              .css('width', value + '%')
+              .append(value + '%')
           )
       );
       $(this._div).append(
@@ -33,29 +50,20 @@ $(function() {
           )
           .css('text-align', 'right')
       );
-    };
+    }
   };
   info.addTo(map);
-
-  // Define markers style
-  var marker = {
-    icon: L.icon({
-      iconUrl: 'img/icon.png',
-      iconSize: [50, 50]
-    })
-  };
 
   // Create base layer
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
 
-
   // Update info control
   function updateInfo(e) {
     info.update(e.target.feature);
     $(info._container).show();
-  };
+  }
   function onEachFeature(feature, layer) {
     layer.on({
       mouseover: updateInfo,
@@ -63,17 +71,17 @@ $(function() {
     });
   }
 
-  // Create features layer
-  function createFeaturesLayer (data) {
-    featuresLayer = L.geoJson(data, {
-      pointToLayer: function (feature, latlng) {
-        return L.marker(latlng, marker);
-      },
-      onEachFeature: onEachFeature
-    }).addTo(map);
-    map.fitBounds(featuresLayer.getBounds(), {padding: [5, 5]});
-  };
-  $.getJSON("tasks.geojson", createFeaturesLayer);
+  // Add projects to features layer
+  function addProjects (data) {
+    $.each(data.features, function (key, project) {
+      $.getJSON("http://tasks.hotosm.org/project/" + project.id + ".json", function (projectData) {
+        project.properties = projectData.properties;
+        featuresLayer.addData(project);
+        map.fitBounds(featuresLayer.getBounds(), {padding: [5, 5]});
+      });
+    });
+  }
+  $.getJSON("tasks.geojson", addProjects);
 
   // Hide zoom control
   $(".leaflet-control-zoom").css("visibility", "hidden");
